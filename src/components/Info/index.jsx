09f224e57df3ba5d8e8f27b072fa2b1125613d09
Tamer idrisov <tamer_idrisov98@mail.ru>
2023-12-28 23:06:43 +0300

    исправил

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
     <section className=" sm:mt-[50px] md:mt-[70px] lg:mt-[150px] w-full lg:h-full lg:flex lg:flex-row flex-col  ">
-    
-    <div className="flex flex-col mt-[1%] lg:gap-6 lg:pl-16 ml-8  ">
-      <h1 className="sm:text-[42px] sm:leading-[65px] sm:font-extrabold  text-[30px] leading-10 font-normal">
-      Что пишут участники <br/>наших путешествий 
-      </h1>
-      <p className="font-normal sm:leading-[40px] sm:text-[18px] text-[15px] ml-2">
-      87% участников приходят по личной <br/>рекомендации от друзей. А каждый 4-ый <br/>путешествует с нами более двух раз!
-      </p>
-      <button className="sm::w-[271px] sm:h-[75px] rounded-[38px] border-[2px] border-cyan-500 text-cyan-500 focus:hover:bg-black lg:text-[20px] lg:font-semibold mt-2 w-[250px] h-[40px] ">
-      Читать отзывы
-      </button>
-    </div>
-    <div className="relative lg:w-[50%] lg:h-full pt-[20px] h-[270px] ">
+      <div className="flex flex-col mt-[1%] lg:gap-6 lg:pl-16 ml-8  ">
+        <h1 className="sm:text-[42px] sm:leading-[65px] sm:font-extrabold  text-[30px] leading-10 font-normal">
+          Что пишут участники <br />
+          наших путешествий
+        </h1>
+        <p className="font-normal sm:leading-[40px] sm:text-[18px] text-[15px] ml-2">
+          87% участников приходят по личной <br />
+          рекомендации от друзей. А каждый 4-ый <br />
+          путешествует с нами более двух раз!
+        </p>
+        <button className="sm::w-[271px] sm:h-[75px] rounded-[38px] border-[2px] border-cyan-500 text-cyan-500 focus:hover:bg-black lg:text-[20px] lg:font-semibold mt-2 w-[250px] h-[40px] ">
+          Читать отзывы
+        </button>
+      </div>
+      <div className="relative lg:w-[50%] lg:h-full pt-[20px] h-[270px] ">
         <div className="absolute -z-10 w-full h-full">
           <img
             className=" lg:pl-[5%] w-full h-full"
@@ -25,19 +27,20 @@ const Info = () => {
         </div>
         <div className=" flex pt-6 flex-col pl-[15%] ">
           <p className=" text-cyan-50">
-          Отзыв
+            Отзыв
             <br />
             Елена Иванова
           </p>
-          <div className=" pt-[230px] pb-[20px]">
+          <div className=" pt-32 lg:pt-[230px] pb-[20px]">
             <p className=" text-cyan-50">
-            Пожалуй, это был самый <br/>лучший отпуск в моей жизни...
+              Пожалуй, это был самый <br />
+              лучший отпуск в моей жизни...
             </p>
           </div>
         </div>
       </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
 export default Info;
